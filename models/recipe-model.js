e871c6f7d0b65845fc30a7e98f2cce454ea7271d
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-10-25 10:37:13 -0400

    SQL translations added to recipe-model file, first three out of four stretch goals met

--- models/recipe-model.js
+++ models/recipe-model.js
@@ -4,6 +4,12 @@ function getRecipes() {
     return db("recipes")
 }
 
+/* getRecipes() translates as:
+
+SELECT *
+FROM recipes
+*/
+
 function getShoppingList(recipe_id) {
     return db("recipes_ingredients as ri")
         .innerJoin("recipes as r", "r.id", "ri.recipe_id")
@@ -15,12 +21,29 @@ function getShoppingList(recipe_id) {
         ])
 }
 
+/* getShoppingList() translates as:
+
+SELECT i.name, ri.quantity
+FROM recipes_ingredients AS ri
+JOIN recipes AS r ON r.id = ri.recipe_id
+JOIN ingredients AS i ON i.id = ri.ingredient_id
+WHERE r.id = ?
+*/
+
 function getInstructions(recipe_id) {
     return db("instructions as i")
         .leftJoin("recipes as r", "r.id", "i.recipe_id")
         .where("r.id", recipe_id)
         .select("i.instruction_number", "i.procedure")
 }
+
+/* getInstructions() translates as:
+
+SELECT i.instruction_number, i.procedure
+FROM instructions AS i
+LEFT JOIN recipes AS r ON r.id = i.recipe_id
+WHERE r.id = ?
+*/
 
 module.exports = {
     getRecipes,
